57374770317c37f19a99f5f27c943d42bf71950f
Momchil Molnar <momchil.molnar@colorado.edu>
2020-11-02 10:39:02 -0700

    Adding test directory

---
+++ test/test_destretch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 21:18:02 2020
+
+Test cases for the destretching routine in destretch.py
+
+@author: molnarad
+"""
+
+
+
+
+if __name__=="__main__":
+    """
+    Run general test case for the code on the provided test.fits file
+    """
+    
+    
